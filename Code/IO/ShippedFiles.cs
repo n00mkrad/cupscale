1383abf214ddac9ed042ce90539ac73ba55b97dc
N00MKRAD <me@nmkd.de>
2020-10-15 11:47:52 +0200

    Update ShippedFiles.cs

--- Code/IO/ShippedFiles.cs
+++ Code/IO/ShippedFiles.cs
@@ -83,17 +83,15 @@ namespace Cupscale.IO
 
 			path7za = Path.Combine(path, "7za.exe");
 			File.WriteAllBytes(path7za, Resources.x64_7za);
-			// File.WriteAllBytes(Path.Combine(path, "nvcompress.exe"), Resources.nvcompress);
-			// File.WriteAllBytes(Path.Combine(path, "nvtt.dll"), Resources.nvtt);
 			File.WriteAllBytes(Path.Combine(IOUtils.GetAppDataDir(), "esrgan.7z"), Resources.esrgan);
 			File.WriteAllBytes(Path.Combine(IOUtils.GetAppDataDir(), "ncnn.7z"), Resources.esrgan_ncnn);
 			File.WriteAllBytes(Path.Combine(IOUtils.GetAppDataDir(), "av.7z"), Resources.av);
 
 			dialog.ChangeText("Installing ESRGAN resources...");
 			await UnSevenzip(Path.Combine(IOUtils.GetAppDataDir(), "esrgan.7z"));
-			dialog.ChangeText("Installing ESRGAN-NCNN resources...");
+			dialog.ChangeText("Installing ESRGAN-NCNN-Vulkan resources...");
 			await UnSevenzip(Path.Combine(IOUtils.GetAppDataDir(), "ncnn.7z"));
-			dialog.ChangeText("Installing FFmpeg resources...");
+			dialog.ChangeText("Installing Audio/Video/Image resources...");
 			await UnSevenzip(Path.Combine(IOUtils.GetAppDataDir(), "av.7z"));
 
 			File.WriteAllText(Path.Combine(IOUtils.GetAppDataDir(), "shipped_files_version"), Resources.shipped_files_version);
