2a4d07dadb6cdb8b8691a2d7f0376c0f349e658c
N00MKRAD <me@nmkd.de>
2020-10-15 11:50:10 +0200

    updated build number

--- Code/Main/MainForm.Designer.cs
+++ Code/Main/MainForm.Designer.cs
@@ -1535,7 +1535,7 @@ namespace Cupscale
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(344, 34);
             this.label5.TabIndex = 1;
-            this.label5.Text = "Cupscale [Build 22 - 10/14/20]";
+            this.label5.Text = "Cupscale [Build 23 - 10/15/20]";
             this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // panel7
