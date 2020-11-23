9f12a12c606bd2704266dfed803b6eff89c8bc97
N00MKRAD <me@nmkd.de>
2020-11-23 22:26:19 +0100

    Whitelisted WEBM and AVI for video upscaling

--- Code/IO/IOUtils.cs
+++ Code/IO/IOUtils.cs
@@ -17,7 +17,7 @@ namespace Cupscale
     internal class IOUtils
     {
         public static string[] compatibleExtensions = new string[] { ".png", ".jpg", ".jpeg", ".bmp", ".tga", ".webp", ".dds", ".gif" };
-        public static string[] videoExtensions = new string[] { ".mp4", ".mkv", ".gif" };
+        public static string[] videoExtensions = new string[] { ".mp4", ".m4v", ".mkv", ".webm", ".gif", ".avi" };
         static bool hasShownPortableInfo = false;
 
         public static string GetAppDataDir()
