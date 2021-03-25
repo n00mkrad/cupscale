3a6da5a738c0af1e630aa3ad4b4b9b0d8fc1441a
N00MKRAD <login@nmkd.de>
2021-03-25 23:42:34 +0100

    Fixed embeded python installer & updated version number

--- Code/OS/EmbeddedPython.cs
+++ Code/OS/EmbeddedPython.cs
@@ -44,9 +44,6 @@ namespace Cupscale.OS
             string shippedPath = Installer.path;
             IOUtils.TryDeleteIfExists(Path.Combine(shippedPath, "py", "utils"));
             await IOUtils.CopyDir(Path.Combine(shippedPath, "utils"), Path.Combine(shippedPath, "py", "utils"));
-            File.Copy(Path.Combine(shippedPath, "esrlupscale.py"), Path.Combine(shippedPath, "py", "esrlupscale.py"), true);
-            File.Copy(Path.Combine(shippedPath, "esrlmodel.py"), Path.Combine(shippedPath, "py", "esrlmodel.py"), true);
-            File.Copy(Path.Combine(shippedPath, "esrlrrdbnet.py"), Path.Combine(shippedPath, "py", "esrlrrdbnet.py"), true);
         }
 
         static TextBox logBox;
