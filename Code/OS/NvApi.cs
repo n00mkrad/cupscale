4c556bff149ef9b4ee03208eec368e941ef07cab
n00mkrad <login@nmkd.de>
2021-08-18 17:16:33 +0200

    List all GPUs with VRAM instead of just the first

--- Code/OS/NvApi.cs
+++ Code/OS/NvApi.cs
@@ -12,9 +12,10 @@ namespace Cupscale.OS
 {
     class NvApi
     {
-        static PhysicalGPU gpu;
-        static float vramGb;
-        static float vramFreeGb;
+        public enum Architecture { Undetected, Fermi, Kepler, Maxwell, Pascal, Turing, Ampere };
+        public static List<PhysicalGPU> gpuList = new List<PhysicalGPU>();
+        private Dictionary<PhysicalGPU, float> vramTotal = new Dictionary<PhysicalGPU, float>();
+        private Dictionary<PhysicalGPU, float> vramAvail = new Dictionary<PhysicalGPU, float>();
 
         public static async void Init ()
         {
@@ -22,9 +23,17 @@ namespace Cupscale.OS
             {
                 NVIDIA.Initialize();
                 PhysicalGPU[] gpus = PhysicalGPU.GetPhysicalGPUs();
+
                 if (gpus.Length == 0)
                     return;
-                gpu = gpus[0];
+
+                gpuList = gpus.ToList();
+                List<string> gpuNames = new List<string>();
+
+                foreach (PhysicalGPU gpu in gpus)
+                    Logger.Log($"Detected Card: {gpu.FullName} / GPU: {gpu.ArchitectInformation.ShortName} / Arch: {GetArch(gpu)}");
+
+                Logger.Log($"Initialized Nvidia API. GPU{(gpus.Length > 1 ? "s" : "")}: {GetGpuListStr()}");
 
                 while (true)
                 {
@@ -38,18 +47,58 @@ namespace Cupscale.OS
             }
         }
 
+        public static string GetGpuListStr ()
+        {
+            return string.Join(", ", gpuList.Select(x => x.FullName));
+        }
+
+        public static bool HasAmpereOrNewer()
+        {
+            foreach (PhysicalGPU gpu in gpuList)
+            {
+                Architecture arch = GetArch(gpu);
+
+                if (arch == Architecture.Ampere || arch == Architecture.Undetected)
+                    return true;
+            }
+
+            return false;
+        }
+
+        public static Architecture GetArch(PhysicalGPU gpu)
+        {
+            string gpuCode = gpu.ArchitectInformation.ShortName;
+
+            if (gpuCode.Trim().StartsWith("GF")) return Architecture.Fermi;
+            if (gpuCode.Trim().StartsWith("GK")) return Architecture.Kepler;
+            if (gpuCode.Trim().StartsWith("GM")) return Architecture.Maxwell;
+            if (gpuCode.Trim().StartsWith("GP")) return Architecture.Pascal;
+            if (gpuCode.Trim().StartsWith("TU")) return Architecture.Turing;
+            if (gpuCode.Trim().StartsWith("GA")) return Architecture.Ampere;
+
+            return Architecture.Undetected;
+        }
+
         public static void RefreshVram ()
         {
-            if (Form.ActiveForm != Program.mainForm || gpu == null)    // Don't refresh if not in focus or no GPU detected
+            if (Form.ActiveForm != Program.mainForm) // Don't refresh if not in focu
                 return;
-            vramGb = (gpu.MemoryInformation.AvailableDedicatedVideoMemoryInkB / 1000f / 1024f);
-            vramFreeGb = (gpu.MemoryInformation.CurrentAvailableDedicatedVideoMemoryInkB / 1000f / 1024f);
-            Color col = Color.White;
-            if (vramFreeGb < 2f)
-                col = Color.Orange;
-            if (vramFreeGb < 1f)
-                col = Color.OrangeRed;
-            Program.mainForm.SetVramLabel($"{gpu.FullName}: {vramGb.ToString("0.00")} GB VRAM - {vramFreeGb.ToString("0.00")} GB Free", col);
+
+            List<string> gpusWithVram = new List<string>();
+
+            foreach(PhysicalGPU gpu in gpuList)
+            {
+                if (gpu == null)
+                    continue;
+
+                float vramGb = (gpu.MemoryInformation.AvailableDedicatedVideoMemoryInkB / 1000f / 1024f);
+                float vramFreeGb = (gpu.MemoryInformation.CurrentAvailableDedicatedVideoMemoryInkB / 1000f / 1024f);
+                string shortenedName = gpu.FullName.Replace("NVIDIA ", "").Replace("AMD ", "");
+
+                gpusWithVram.Add($"{shortenedName}: {vramFreeGb.ToString("0.0")}/{vramGb.ToString("0.0")} GB Free");
+            }
+
+            Program.mainForm.SetVramLabel(string.Join(" - ", gpusWithVram), Color.White);
         }
 
         public static string GetGpuName ()
